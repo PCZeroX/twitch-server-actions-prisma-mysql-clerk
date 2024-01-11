ddc2b830cff2824051f1b748c05d02cccef4389e
pczerox <pczeroxstore@gmail.com>
2024-01-10 21:46:20 -0500

    Video component

--- src/components/stream/stream-player/index.tsx
+++ src/components/stream/stream-player/index.tsx
@@ -10,6 +10,7 @@ import { useChatSidebar } from "@/store/use-chat-sidebar";
 import { useViewerToken } from "@/hooks/use-viewer-token";
 
 import { ChatToggle } from "./chat-toggle";
+import { Video, VideoSkeleton } from "./video";
 
 // type CustomStreamType = {
 //   id: string;
@@ -74,8 +75,22 @@ export const StreamPlayer = ({
           collapsed && "lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2"
         )}
       >
-        Allowed to watch the stream
+        <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
+          <Video hostName={user.username} hostIdentity={user.id} />
+        </div>
       </LiveKitRoom>
     </>
+  );
+};
+
+export const StreamPlayerSkeleton = () => {
+  return (
+    <div className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full">
+      <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
+        <VideoSkeleton />
+        {/* <HeaderSkeleton /> */}
+      </div>
+      <div className="col-span-1 bg-background">{/* <ChatSkeleton /> */}</div>
+    </div>
   );
 };
